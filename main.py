e533e807280b53ac6d988461ee5e63384cb07453
KAKKY4876 <angrykakky@gmail.com>
2026-03-11 05:31:49 +0900

    Add icon

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
 import sqlite3
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/battle_logs")
 def get_battlelogs():
